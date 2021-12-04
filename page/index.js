c066f808465b234cd7ce4d6eb945352f4e1f8209
Artyom Rudenko <Artrudjob@yandex.ru>
2021-12-04 16:11:42 +0300

    feat: реализовать переход от формы2 к форме 3

--- page/index.js
+++ page/index.js
@@ -24,3 +24,35 @@ function addElementToMainInfo(element) {
 }
 
 addElementToMainInfo(getWelcomeScreenElement());
+
+
+const barForm = document.querySelector('#bar').cloneNode(true).content;
+const main = document.querySelector('.main');
+main.prepend(barForm);
+
+const barSubmit = document.querySelector('#bar-submit');
+
+function addElementobshepitForm() {
+    const obshepitForm = document.querySelector('#obshepitForm').cloneNode(true).content;
+    const barForm = barSubmit.closest('.form-container');
+
+    const selectCity = document.querySelector('#city').value;
+    const placeName = document.querySelector('#place-name').value;
+    const contactName = document.querySelector('#contact-name').value;
+    const contactPhone = document.querySelector('#contact-phone').value;
+    const desc = document.querySelector('#desc').value;
+
+    if (selectCity.length == 0 || placeName.length == 0 || contactName.length == 5 || contactPhone.length == 11 || desc.length == 0) {
+        return false;
+    } else {
+        main.prepend(obshepitForm);
+        barForm.remove();
+    }
+}
+
+
+function barFormSubmitHandler (evt) {
+    evt.preventDefault();
+}
+
+barSubmit.addEventListener('click', addElementobshepitForm, barFormSubmitHandler);
