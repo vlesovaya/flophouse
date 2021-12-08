40e121c408419b5efd16cd128298740a311a14c5
ElenaSolov <90965924+ElenaSolov@users.noreply.github.com>
2021-12-08 10:40:10 +0200

    feat: Реализовать маску для инпута телефона (#23)

--- page/index.js
+++ page/index.js
@@ -1,4 +1,6 @@
 const mainInfoContainer = document.querySelector('.main__info');
+let keyCode;
+
 
 // Get elements from templates
 
@@ -83,6 +85,14 @@ function getFormById(formId, onSubmitElement, onResetElement) {
         evt.preventDefault();
         addElementToMainInfo(onResetElement());
     });
+
+    const phoneInput = formElement.querySelector('.form-container__text-input[type="tel"]');
+    if (phoneInput) {
+        phoneInput.addEventListener("input", mask, false);
+        phoneInput.addEventListener("focus", mask, false);
+        phoneInput.addEventListener("blur", mask, false);
+        phoneInput.addEventListener("keydown", mask, false);
+    }
     return templateElement;
 }
 
@@ -98,5 +108,31 @@ function getCheckedRadioButtonId(form, radioButtonClass) {
     }
     return returnId;
 }
+
+function mask(event) {
+    event.keyCode && (keyCode = event.keyCode);
+    let pos = this.selectionStart;
+    if (pos < 3) event.preventDefault();
+    let matrix = "+7 (___) ___ ____",
+        i = 0,
+        def = matrix.replace(/\D/g, ""),
+        val = this.value.replace(/\D/g, ""),
+        new_value = matrix.replace(/[_\d]/g, function (a) {
+            return i < val.length ? val.charAt(i++) || def.charAt(i) : a
+        });
+    i = new_value.indexOf("_");
+    if (i != -1) {
+        i < 5 && (i = 3);
+        new_value = new_value.slice(0, i)
+    }
+    let reg = matrix.substr(0, this.value.length).replace(/_+/g,
+        function (a) {
+            return "\\d{1," + a.length + "}"
+        }).replace(/[+()]/g, "\\$&");
+    reg = new RegExp("^" + reg + "$");
+    if (!reg.test(this.value) || this.value.length < 5 || keyCode > 47 && keyCode < 58) this.value = new_value;
+    if (event.type == "blur" && this.value.length < 5) this.value = ""
+}
+
 
 addElementToMainInfo(getWelcomeScreenElement());
